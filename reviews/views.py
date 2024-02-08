5fb5f105fbfffa38268df79f3d16133afa5eb851
MaximilianoSanLa <maximiliano.sanchez.lara@gmail.com>
2024-02-08 12:44:35 -0500

    busqueda de peliculas

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import reviews
 # Create your views here.
 def home(request):
-    return render(request,'home.html', {'name':'Greg Lim'})
+    searchTerm= request.GET.get('searchReview')
+    if searchTerm:
+        Reviews= reviews.objects.filter(title_icontains=searchTerm)
+    else:
+        Reviews= reviews.objects.all()
+    return render(request,'home.html', {'searchTerm':searchTerm, 'Reviews':Reviews})
 def about(request):
     return render(request, 'about.html')
 
